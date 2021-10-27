7c0067be2a6bce8ffa480dad941d228e643c7169
shubhamfuloria <sfuloria10200@gmail.com>
2021-10-27 17:41:01 +0530

    popup box

---
+++ popup/app.js
@@ -0,0 +1,16 @@
+const formEl = document.querySelector('.form');
+const loginBtnEl = document.querySelector('.open-popup');
+const bodyEl = document.querySelector('body');
+
+loginBtnEl.addEventListener('click', function () {
+    formEl.classList.toggle('form-open');
+})
+
+formEl.addEventListener('transitionend', function () {
+    bodyEl.classList.toggle('blur');
+})
+
+bodyEl.addEventListener('click', function(){
+    // formEl.classList.toggle('form-open');
+})
+
